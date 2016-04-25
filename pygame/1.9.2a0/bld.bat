e8e35a45e82f768d497db7844425793d6fd1ddfd
Daniel Schreij <dschreij@gmail.com>
2016-04-25 22:23:46 +0200

    Started with updating and adding packages

--- pygame/1.9.2a0/bld.bat
+++ pygame/1.9.2a0/bld.bat
@@ -1,9 +1,10 @@
+@echo off
 rem Building pygame for windows is hell, so we use the wheel files kindly provided
 rem by dr. Gohlke: http://www.lfd.uci.edu/~gohlke/. It is assumed they are placed in
 rem the C:\Libraries directory
 
-@echo off
 c:
 cd \Libraries
-pip install pygame-1.9.2a0-cp%CONDA_PY%-none-win_amd64.whl
+if %ARCH% == 64 pip install pygame-1.9.2a0-cp%CONDA_PY%-none-win_amd64.whl
+if %ARCH% == 32 pip install pygame-1.9.2a0-cp%CONDA_PY%-none-win32.whl
 if errorlevel 1 exit 1
